d39fe7d93c0e27b36285879f2eb5868cd52af2f8
nikispa <88911601+nikispa@users.noreply.github.com>
2022-05-27 01:34:24 +0200

    fixed name case

--- assets/js/crosstable.js
+++ assets/js/crosstable.js
@@ -1,14 +1,16 @@
 
 function run() {
-	var player1 = document.getElementById("player1").value.toLowerCase();
-    var player2 = document.getElementById("player2").value.toLowerCase();
+	let player1 = document.getElementById("player1").value.toLowerCase();
+    let player2 = document.getElementById("player2").value.toLowerCase();
+	let player1c = document.getElementById("player1").value;
+    let player2c = document.getElementById("player2").value;
     let url = `https://lichess.org/api/crosstable/${player1}/${player2}`;
 
     fetch(url)
     	.then(response => response.json())
     	.then(data => {
-            document.getElementById("player1name").innerText = player1 + ":";
-            document.getElementById("player2name").innerText = player2 + ":";
+            document.getElementById("player1name").innerText = player1c + ":";
+            document.getElementById("player2name").innerText = player2c + ":";
             document.getElementById("totalgames").innerText = data.nbGames + " Games";
 
             document.getElementById("player1wins").innerText = `${data.users[player1]} Wins`;
