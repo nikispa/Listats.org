31b21d60eefe864ee0eb0160742918d853d2fe92
nikispa <88911601+nikispa@users.noreply.github.com>
2022-06-06 02:45:19 +0200

    remove consolelogs

--- assets/js/enter.js
+++ assets/js/enter.js
@@ -2,8 +2,6 @@
 window.onload = function(){
 	let indexsearchbar = document.getElementById("indexsearchbar");
 
-	console.log("Searchbar is: " + indexsearchbar);
-	
 	indexsearchbar.addEventListener("keyup", (e) => {
 		if (e.key === "Enter") {
 			searchHome();
